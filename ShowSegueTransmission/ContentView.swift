59699b71af1952212d0fa4a2f7ce0272d168d2a3
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-08 12:07:04 +0300

    Initial Commit

---
+++ ShowSegueTransmission/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShowSegueTransmission
+//
+//  Created by Ömer Faruk Kılıçaslan on 8.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowSegueTransmission/ShowSegueTransmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowSegueTransmissionApp.swift
+//  ShowSegueTransmission
+//
+//  Created by Ömer Faruk Kılıçaslan on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShowSegueTransmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
